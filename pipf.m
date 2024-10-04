6e4c0549096ebb5fe738e206bc2768bc39c1c833
thoegerek <148452003+thoegerek@users.noreply.github.com>
2024-10-04 11:30:20 +0200

    readded functionality to the pipf function

--- pipf.m
+++ pipf.m
@@ -1,16 +1,16 @@
 function [f,eqs,stability] = pipf(ax,ay,b,c,d,mu,nump,uppercutoff)
 %pip as a function
 
-M = fourLinearisedInvader(false);
+M = linearisedInvader(false);
 f = zeros(nump,nump,2);
 
 dtau = ((b-mu)/c)/(nump-1) * uppercutoff;
 tau = 0:dtau:(b-mu)/c *uppercutoff;
-
+%%
 X = zeros(nump,2);
 Y = zeros(nump,2);
 for r = 1:nump
-    [eqx,eqy,stability] = fourEquilibriumsStability(ax,ay,b,c,d,mu,tau(r),true); %This is very slow - try to do this from (x0,0) instead of globally?
+    [eqx,eqy,stability] = equilibriumsStability(ax,ay,b,c,d,mu,tau(r),true); %This is very slow - try to do this from (x0,0) instead of globally?
     nStab = sum(stability == -1);
     X(r,1:nStab) = eqx(stability == -1);
     Y(r,1:nStab) = eqy(stability == -1);
@@ -20,7 +20,7 @@ for r = 1:nump
     
     for j = 1:2
         for i = 1:nump
-            [~,lambda] = eig(M(a1,a2,a3,a4,b,c,d,mu,n1r,n2r,n3r,n4r,X(r,j),Y(r,j),tau(i),tau(r)));
+            [~,lambda] = eig(M(ax,ay,b,c,d,mu,tau(i),tau(r),X(r,j),Y(r,j)));
             f(i,r,j) = max(diag(lambda));
         end
         if nStab == 1
@@ -31,8 +31,8 @@ for r = 1:nump
 
     disp([num2str(r) ' / ' num2str(nump)])
 end
-
-eqs = 0;
+%%
+eqs = [];
 stability = [];
 for j = 1:size(X,2)
     dfd = zeros(nump-1,1);
@@ -43,7 +43,7 @@ for j = 1:size(X,2)
     end
     dfd(1) = [];
     [~,idx] = min(abs(dfd));
-    eqs(1) = tau(idx);
+    taupol = tau(idx);
     fisx = dfd(1:end-1).*dfd(2:end);
     eqidx = find(fisx<0);
     eqs = [eqs tau(eqidx + 1)];
